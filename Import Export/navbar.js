533d975f53d106d362b63e975dc1d4c397c09e5c
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2021-10-22 02:25:43 +0530

    food app assignment

---
+++ Import Export/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+    return `<h3><a href="home.html"><img src="https://images.squarespace-cdn.com/content/v1/5a957c6c5b409b2679594cea/1525207952963-0M9WZ3B26RSMBFOZGN4F/image-asset.png" alt="" width="40px"> PAL Foods</a></h3>
+    <h3><div id="search">
+        <input type="text" id="inp">
+        <button id="btn">Search</button>
+    </div>
+    <div id="sug"></div>
+    </h3>
+    <div id="option">
+        <h3><a href="recipe.html">Recipe Of The Day</a></h3>
+        <h3><a href="home.html">Home</a></h3>
+    </div>`
+}
+
+export default navbar
